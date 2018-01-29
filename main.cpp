d7bef8bd9c69a0cfd31aca2ba96bd691e1db42ea
Laptop <Blychovsky@gmail.com>
2018-01-29 22:29:39 +0200

    1.2

--- main.cpp
+++ main.cpp
@@ -11,9 +11,16 @@ int MyStrlen(const char *);
 void MyStrcpy(char *, const char *);
 int MyStrcmp(const char *, const char *);
 
+void MyCut(const char* , char*, const unsigned int index = 0, const unsigned int count = 1);
+char * MyDCut(const char*, const unsigned int index = 0, const unsigned int count = 1);
 
-int main()
-{
+
+int main(){
+
+    char s1[20], s2[20];
+    int startIndex = 0, size = 0;
+
+
     cout << "Hello World!" << endl;
     cout << MyStrlen("Hello World!") << endl;
 
@@ -22,12 +29,30 @@ int main()
 
     cout << str << endl;
 
-    cout << "enter cstrings to compare: \n";
+    // MyCat demo
+    cout << "cut(): ";
+    cin >> s1, cin >> startIndex, cin >> size;
+    MyCut(s1, s2, startIndex, size);
+    cout << s2 << endl;
+
+    //MyDCat demo
+    cout << "dynamic cut: ";
+    cin >> s1, cin >> startIndex, cin >>size;
+    char * ptr; //for string in heap
+    ptr = MyDCut(s1, startIndex, size);
+    cout << ptr << endl;
+    delete ptr;
+
+
+
+    //compare demo
     while(cin){
-        char s1[20], s2[20];
+        cout << "enter cstrings to compare: \n";
         cin >> s1, cin >> s2;
         cout << MyStrcmp(s1, s2) << endl;
     }
+
+    cin.clear();
 
     return 0;
 }
@@ -68,4 +93,45 @@ int MyStrcmp(const char * arg0, const char *arg1){
         return -1;
     return 0;
 }
+
+/*
+ * cut cstring
+ */
+void MyCut(const char * arg0, char * arg1, const unsigned int index, const unsigned int count){
+    unsigned int size = index+count;
+    for(unsigned int i = index; i < size; ++i){
+        *arg1++ = *(arg0 + i);
+    }
+}
+
+/*
+ * cut cstring using heap
+ */
+char * MyDCut(const char * arg0, const unsigned int index, const unsigned int count){
+    char * result = new char[20];
+    if(!result){
+        cout << "alloc failed" << endl;
+        return nullptr;
+    }
+    //unsigned int size = index+count;
+
+    for(unsigned int i = 0; i < count; ++i){
+        result[i] = arg0[index+i];
+    }
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
